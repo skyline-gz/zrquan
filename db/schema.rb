066f9c492e6669f4bcf2e2ef5e66434ce2631a59
James-Ouyang <dreamou@gmail.com>
2014-05-08 15:35:17 +0800

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,81 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140507070617) do
+ActiveRecord::Schema.define(version: 20140507085023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: true do |t|
+    t.text     "content"
+    t.integer  "agree_score"
+    t.integer  "user_id"
+    t.integer  "question_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "commentable_id"
+    t.string   "commentable_type"
+    t.integer  "comment_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id", "commentable_type"], name: "index_comments_on_commentable_id_and_commentable_type", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "experience_articles", force: true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.integer  "agree_score"
+    t.integer  "theme_id"
+    t.integer  "industry_id"
+    t.integer  "category_id"
+    t.boolean  "mark_flag"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "experience_articles", ["category_id"], name: "index_experience_articles_on_category_id", using: :btree
+  add_index "experience_articles", ["industry_id"], name: "index_experience_articles_on_industry_id", using: :btree
+  add_index "experience_articles", ["theme_id"], name: "index_experience_articles_on_theme_id", using: :btree
+  add_index "experience_articles", ["user_id"], name: "index_experience_articles_on_user_id", using: :btree
+
+  create_table "messages", force: true do |t|
+    t.text     "content"
+    t.integer  "type"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
+
+  create_table "questions", force: true do |t|
+    t.string   "title",       null: false
+    t.text     "content"
+    t.integer  "theme_id",    null: false
+    t.integer  "industry_id"
+    t.integer  "category_id"
+    t.integer  "answer_num"
+    t.boolean  "mark_flag"
+    t.integer  "user_id",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "questions", ["category_id"], name: "index_questions_on_category_id", using: :btree
+  add_index "questions", ["industry_id"], name: "index_questions_on_industry_id", using: :btree
+  add_index "questions", ["theme_id"], name: "index_questions_on_theme_id", using: :btree
+  add_index "questions", ["user_id"], name: "index_questions_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
