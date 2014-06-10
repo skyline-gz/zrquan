ac48dce22ff399477eb6e3d10e87897f86348dd0
James-Ouyang <dreamou@gmail.com>
2014-06-10 16:27:53 +0800

    changed .gitignore

--- app/controllers/consult_subjects_controller.rb
+++ app/controllers/consult_subjects_controller.rb
@@ -25,6 +25,11 @@ class ConsultSubjectsController < ApplicationController
   # POST /consult_subjects.json
   def create
     @consult_subject = ConsultSubject.new(consult_subject_params)
+		logger.debug("params:" + consult_subject_params)
+		@consult_subject.apprentice_id = current_user.id
+		@consult_subject.mentor_stat_flag = 1		# applying
+		@consult_subject.user_stat_flag = 1			# applying
+
 
     respond_to do |format|
       if @consult_subject.save
@@ -69,6 +74,6 @@ class ConsultSubjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def consult_subject_params
-      params.require(:consult_subject).permit(:title, :content, :theme_id, :mentor_id, :apprentice_id, :mentor_stat_flag, :user_stat_flag)
+      params.require(:consult_subject).permit(:title, :content, :theme_id, :mentor)
     end
 end
