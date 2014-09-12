b334f15a3bbf36c2b07a91a86c887f19734881b5
James-Ouyang <dreamou@gmail.com>
2014-09-12 16:38:33 +0800

    added change password function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,18 +4,12 @@ class HomeController < ApplicationController
 	def home
 		@rec_mentors = RecommendMentor.all
 		@news_feeds = NewsFeed.all
-		#@recent_activities = Activity.
-		#		joins(
-		#		"INNER JOIN relationships r ON 
-		#		r.following_user_id = activities.user_id AND 
-		#		r.follower_id = " + current_user.id.to_s).
-		#		where("recent_flag = true").
-		#		order("id desc")
-
-		@recent_activities = Activity.find_by_sql(
-					"select A.* from ACTIVITIES A inner join RELATIONSHIPS R on
-					 A.USER_ID = R.FOLLOWING_USER_ID and R.FOLLOWER_ID = " + current_user.id.to_s + "
-					 where A.RECENT_FLAG = true order by A.ID DESC")
+		if current_user != nil
+			@recent_activities = Activity.find_by_sql(
+				"select A.* from ACTIVITIES A inner join RELATIONSHIPS R on
+				 A.USER_ID = R.FOLLOWING_USER_ID and R.FOLLOWER_ID = " + current_user.id.to_s + "
+				 where A.RECENT_FLAG = true order by A.ID DESC")
+		end
 	end
 
 	def search
