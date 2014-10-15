d78a529b816c825384cf49c08231bc6f21ab6ed2
vicky <yuqi.fan@foxmail.com>
2014-10-15 15:03:11 +0800

    统一代码风格

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,25 +6,25 @@ class User < ActiveRecord::Base
 	
 	mount_uploader :avatar, AvatarUploader
 
-  has_many:questions
-  has_many:answers
-  has_many:articles
-  has_many:messages
-  has_many:m_subjects, class_name: "ConsultSubject", foreign_key: "mentor_id"
-  has_many:u_subjects, class_name: "ConsultSubject", foreign_key: "apprentice_id"
-	has_many:consult_replies
-  has_many:comments
-	has_many:private_messages
-	has_many:invitations, foreign_key: "mentor_id"
-	has_many:invited_questions, class_name: "Question", through: :invitations, source: :question
-	has_many:bookmarks
-	has_many:agreements
-	has_many:relationships, foreign_key: "follower_id"
-	has_many:following_users, class_name: "User", through: :relationships
-	has_many:reverse_relationships, class_name: "Relationship", foreign_key: "following_user_id"
-	has_many:followers, class_name: "User", through: :reverse_relationships
-  has_one:user_setting
-	has_many:activities
+  has_many :questions
+  has_many :answers
+  has_many :articles
+  has_many :messages
+  has_many :m_subjects, class_name: "ConsultSubject", foreign_key: "mentor_id"
+  has_many :u_subjects, class_name: "ConsultSubject", foreign_key: "apprentice_id"
+	has_many :consult_replies
+  has_many :comments
+	has_many :private_messages
+	has_many :invitations, foreign_key: "mentor_id"
+	has_many :invited_questions, class_name: "Question", through: :invitations, source: :question
+	has_many :bookmarks
+	has_many :agreements
+	has_many :relationships, foreign_key: "follower_id"
+	has_many :following_users, class_name: "User", through: :relationships
+	has_many :reverse_relationships, class_name: "Relationship", foreign_key: "following_user_id"
+	has_many :followers, class_name: "User", through: :reverse_relationships
+  has_one :user_setting
+	has_many :activities
 
 	def following_num
 		following_users.count
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
 		@relationship.following_user_id = other_user.id
 		@relationship.save!
 		logger.debug("relationship saved")
-		if user_setting.followed_flag == true
+		if user_setting.followed_flag
 			logger.debug("ready to send message")
 			@relationship.following_user.messages.create!(msg_type: 10, extra_info1_id: id, extra_info1_type: "User")
 			logger.debug("message created")
--- app/models/user_setting.rb
+++ app/models/user_setting.rb
@@ -1,3 +1,3 @@
 class UserSetting < ActiveRecord::Base
-	belongs_to:user
+	belongs_to :user
 end
