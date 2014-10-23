3b01cc03515a8e217b0f2cff0cf301332d47e16f
James-Ouyang <dreamou@gmail.com>
2014-10-23 14:50:11 +0800

    增加了question的测试用例（排序）

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -26,5 +26,27 @@ RSpec.describe Question, :type => :model do
       expect(sorted_answers[0]).to eq(answer_score_25)
       expect(sorted_answers[1]).to eq(answer_score_20)
     end
+
+    it "multiple other users' answers should sorted by agree scores desc" do
+      answer_score_30 = FactoryGirl.create(:q1_other_answer_1, :question=>question)
+      answer_score_40 = FactoryGirl.create(:q1_other_answer_2, :question=>question)
+      sorted_answers = question.sorted_answers
+      expect(sorted_answers[0]).to eq(answer_score_40)
+      expect(sorted_answers[1]).to eq(answer_score_30)
+    end
+
+    it "should sort answers according to the rules above when there multiple invited answers and normal answers" do
+      invitation_1
+      invitation_2
+      invited_answer_score_20 = FactoryGirl.create(:q1_invited_answer_1, :question=>question, :user=>mentor_1)
+      invited_answer_score_25 = FactoryGirl.create(:q1_invited_answer_2, :question=>question, :user=>mentor_2)
+      other_answer_score_30 = FactoryGirl.create(:q1_other_answer_1, :question=>question)
+      other_answer_score_40 = FactoryGirl.create(:q1_other_answer_2, :question=>question)
+      sorted_answers = question.sorted_answers
+      expect(sorted_answers[0]).to eq(invited_answer_score_25)
+      expect(sorted_answers[1]).to eq(invited_answer_score_20)
+      expect(sorted_answers[2]).to eq(other_answer_score_40)
+      expect(sorted_answers[3]).to eq(other_answer_score_30)
+    end
   end
 end
