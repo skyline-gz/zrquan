50a750d1e76d512e3ddecf664a09fe911cced17d
James-Ouyang <dreamou@gmail.com>
2014-10-24 16:05:34 +0800

    增加了一些factories和补充ability的测试用例

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,17 +1,18 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :published_article, class: Article do
+  factory :article do
     association :user, factory: :normal_user_1
     title       "经验题目"
     content     "经验内容"
-    draft_falg  false
+
+    factory :published_article do
+      draft_flag  false
+    end
+
+    factory :draft_article do
+      draft_flag  true
+    end
   end
 
-  factory :draft_article, class: Article do
-    association :user, factory: :normal_user_1
-    title       "草稿经验题目"
-    content     "草稿经验内容"
-    draft_falg  true
-  end
 end
--- spec/factories/consult_subjects.rb
+++ spec/factories/consult_subjects.rb
@@ -2,5 +2,25 @@
 
 FactoryGirl.define do
   factory :consult_subject do
+    title       "咨询主题题目"
+    content     "咨询主题内容"
+    association :apprentice, factory: :normal_user_1
+    association :mentor, factory: :mentor_1
+
+    factory :applying_subject do
+      stat_class  1
+    end
+
+    factory :in_progress_subject do
+      stat_class  2
+    end
+
+    factory :closed_subject do
+      stat_class  3
+    end
+
+    factory :ignored_subject do
+      stat_class  4
+    end
   end
 end
