f1ca412f07d88882c3dc0f6f3ea1318c912f070a
vicky <yuqi.fan@foxmail.com>
2014-10-28 11:13:13 +0800

    登录弹出框实现，代码构建

--- front/src/scripts/main.js
+++ front/src/scripts/main.js
@@ -28,12 +28,27 @@ require([
     ) {
     'use strict';
     $(document).ready(function() {
+        var authModal = $('#authModal');
         $('#btn-sign-up').click(function(){
-            $('#authModal').modal('show');
+            $('div[role=sign-in]', authModal).hide();
+            $('div[role=sign-up]', authModal).show();
+            authModal.modal('show');
         });
 
         $("#btn-sign-in").click(function(){
-            $('#myModal').modal('show');
+            $('div[role=sign-in]', authModal).show();
+            $('div[role=sign-up]', authModal).hide();
+            authModal.modal('show');
+        });
+
+        $("#btn-switch-sign-up").click(function(){
+            $('div[role=sign-in]', authModal).hide();
+            $('div[role=sign-up]', authModal).show();
+        });
+
+        $("#btn-switch-sign-in").click(function(){
+            $('div[role=sign-in]', authModal).show();
+            $('div[role=sign-up]', authModal).hide();
         });
     });
 });
