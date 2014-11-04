0e291842b251efffd91e4a6c67f3673a1989d4b0
vicky <yuqi.fan@foxmail.com>
2014-11-04 11:31:11 +0800

    增加grunt watch的支持，自动发布静态资源到rails assets目录

--- front/Gruntfile.js
+++ front/Gruntfile.js
@@ -10,10 +10,10 @@ module.exports = function(grunt){
                     require: 'compass/import-once/activate',
                     outputStyle: 'expanded',
                     sassDir: 'src/sass',
-                    cssDir: '.development/stylesheets',
+                    cssDir: 'src/css',
                     imagesDir: 'src/images',
                     imagesPath: 'src/images',
-                    generatedImagesDir: 'development/images',
+                    generatedImagesDir: 'src/images',
                     environment: 'development',
                     specify: 'src/sass/<%= pkg.name %>.scss',
                     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -65,15 +65,31 @@ module.exports = function(grunt){
             imagesSrcToDev: {
                 expand: true,
                 cwd: 'src/images/',
-                src: ['**'],
+                src: ['**','!**/icons/**', '!**/avatars/**', '!**/big-icons/**'],
                 dest: 'development/images/'
             },
 
-            //发布到rails的web目录部署
+            //复制所需文件到front/development目录
+            toDev: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/images/',
+                        src: ['**','!**/icons/**', '!**/avatars/**', '!**/big-icons/**'],
+                        dest: 'development/images/'
+                    },
+                    { src:"src/css/zrquan.css", dest:"development/stylesheets/zrquan.css"},
+                    { src:"src/vendors/requirejs/require.js", dest:"development/javascripts/require.js" },
+                    { src:"src/vendors/html5shiv/html5shiv.js", dest:"development/javascripts/html5shiv.js" }
+                ]
+            },
+
+            //从development目录发布到rails的web目录部署
             development: {
-                expand: false,
-                cwd: 'development/',
-                src: ['/**'],
+                expand: true,
+                cwd: 'development',
+                //需要exclude已经在compass中做成雪碧图的文件目录,见src/sass/_bass.scss定义
+                src: ['**','!**/icons/**', '!**/avatars/**', '!**/big-icons/**'],
                 dest: '../app/assets/'
             }
         },
@@ -93,6 +109,27 @@ module.exports = function(grunt){
                 src: 'src/vendors/html5shiv/html5shiv.js',
                 dest: 'development/javascripts/html5shiv.min.js'
             }
+        },
+        //监视源文件的改动，并重新构建
+        //Ubuntu 需执行下面命令，放开系统对inotify的文件数量限制
+        // echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+        watch: {
+            scripts: {
+                files: ['src/scripts/*.js'],
+                tasks: ['default'],
+                options: {
+                    debounceDelay: 500,
+                    interrupt: true
+                }
+            },
+            sass: {
+                files: ['src/sass/*.scss'],
+                tasks: ['default'],
+                options: {
+                    debounceDelay: 500,
+                    interrupt: true
+                }
+            }
         }
     });
 
@@ -100,8 +137,9 @@ module.exports = function(grunt){
     grunt.loadNpmTasks('grunt-contrib-requirejs');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // 默认任务(发布到开发环境)
     grunt.registerTask('default', ['compass:development', 'requirejs:development',
-        'copy:imagesSrcToDev', 'copy:development']);
+        'copy:toDev', 'copy:development']);
 };
