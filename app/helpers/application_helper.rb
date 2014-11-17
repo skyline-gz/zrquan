631f8ad462a044225cf539e0217f5314ad7491d4
vicky <yuqi.fan@foxmail.com>
2014-11-17 13:25:31 +0800

    登陆导航栏　代码构建

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # 生成姓名
+  def generate_name(first_name, last_name)
+    if check_english(first_name)|| check_english(last_name)
+      first_name + ' ' + last_name
+    else
+      first_name + last_name
+    end
+  end
+
+  private
+  def check_english(str)
+    /\A[a-zA-Z]+\z/.match(str) != nil
+  end
 end
