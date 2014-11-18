cfd5126a5805c2bdd0c4578d727bc9541183310e
vicky <yuqi.fan@foxmail.com>
2014-11-18 16:52:57 +0800

    视图管理，代码构建

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,9 +1,8 @@
 Zrquan.module('Navbar', function(Module, App, Backbone, Marionette, $, _){
     'use strict';
 
-    var authModal = $('#authModal');
-    var activateModal = $('#activateModal');
-    var forgetPasswordModal = $('#forgetPasswordModal');
+    var authModalView, activateModalView, forgetPasswordModalView;
+    var navbarEventBus = new Backbone.Wreqr.EventAggregator();
 
     Module.addInitializer(function() {
         console.log("Module Navbar init...");
@@ -11,7 +10,7 @@ Zrquan.module('Navbar', function(Module, App, Backbone, Marionette, $, _){
         Module.controller.start();
     });
 
-
+    //导航栏主视图
     Module.View = Marionette.LayoutView.extend({
         el: "div[role=navigation]",
         events: {
@@ -20,14 +19,16 @@ Zrquan.module('Navbar', function(Module, App, Backbone, Marionette, $, _){
             'mouseover .user-link': 'onShowProfileDropdown'
         },
         onClickBtnSignUp: function(e) {
-            this.$('div[role=sign-in]', authModal).hide();
-            this.$('div[role=sign-up]', authModal).show();
-            authModal.modal('show');
+//            this.$('div[role=sign-in]', authModal).hide();
+//            this.$('div[role=sign-up]', authModal).show();
+//            authModal.modal('show');
+            navbarEventBus.trigger("modal:show", "authModal");
         },
         onClickBtnSignIn: function(e) {
             this.$('div[role=sign-in]', authModal).show();
             this.$('div[role=sign-up]', authModal).hide();
-            authModal.modal('show');
+//            authModal.modal('show');
+            navbarEventBus.trigger("modal:show", "authModal");
         },
         onShowProfileDropdown: function(e) {
             this.$("#top-nav-profile-dropdown").show();
@@ -43,10 +44,59 @@ Zrquan.module('Navbar', function(Module, App, Backbone, Marionette, $, _){
         start: function() {
             this.view = new Module.View();
             this.view.render();
+            authModalView.render();
+            activateModalView.render();
+            forgetPasswordModalView.render();
         }
     });
 
-    //private function
+    //模态框抽象视图
+    var ModalView = Backbone.Marionette.ItemView.extend({
+        modalName: "",
+        showModal: function(modalName) {
+            console.log(modalName + " show");
+            if(modalName && modalName == this.modalName) {
+                this.$el.modal("show");
+            }
+        },
+        hideModal: function() {
+            this.$el.modal("hide");
+        },
+        initialize: function() {
+            this.listenTo(navbarEventBus, 'modal:show', this.showModal);
+            this.listenTo(navbarEventBus, 'modal:hide', this.showModal);
+        },
+        render: function() {
+            this.bindUIElements(); // wire up this.ui, if any
+            console.log(this.modalName + " Modal View Init...")
+        }
+    });
 
+    //登陆注册模态框视图
+    authModalView = new (ModalView.extend({
+        el: "#authModal",
+        modalName: "authModal",
+        events: {
+
+        }
+    }))();
+
+    //激活成功模态框视图
+    activateModalView = new (ModalView.extend({
+        el: "#activateModal",
+        modalName: "activateModal",
+        events: {
+
+        }
+    }))();
+
+    //忘记密码模态框视图
+    forgetPasswordModalView = new (ModalView.extend({
+        el: "#forgetPasswordModal",
+        modalName: "forgetPasswordModal",
+        events: {
+
+        }
+    }))();
 
 });
