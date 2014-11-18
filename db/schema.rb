cf542beee072d40e6b69b3fda9c2bd857052f70e
James-Ouyang <dreamou@gmail.com>
2014-11-18 15:25:35 +0800

    修改了user的model，增加了“最近的公司，职位，最近的学历”等字段

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141118060118) do
+ActiveRecord::Schema.define(version: 20141118072121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -418,12 +418,12 @@ ActiveRecord::Schema.define(version: 20141118060118) do
   add_index "user_settings", ["user_id"], name: "index_user_settings_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -439,15 +439,21 @@ ActiveRecord::Schema.define(version: 20141118060118) do
     t.integer  "gender"
     t.integer  "province_id"
     t.integer  "city_id"
+    t.integer  "latest_company_id"
+    t.string   "latest_position"
+    t.integer  "latest_school_id"
+    t.string   "latest_major"
     t.string   "signature"
     t.text     "description"
-    t.boolean  "verified_flag",          default: false
+    t.boolean  "verified_flag"
     t.string   "avatar"
   end
 
   add_index "users", ["city_id"], name: "index_users_on_city_id", using: :btree
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["latest_company_id"], name: "index_users_on_latest_company_id", using: :btree
+  add_index "users", ["latest_school_id"], name: "index_users_on_latest_school_id", using: :btree
   add_index "users", ["province_id"], name: "index_users_on_province_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
