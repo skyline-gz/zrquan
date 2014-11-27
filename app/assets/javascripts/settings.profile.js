b96618e7a8df78d91354a677fed31d17ab5281c0
vicky <yuqi.fan@foxmail.com>
2014-11-27 15:48:01 +0800

    公司名称自动匹配，代码构建

--- app/assets/javascripts/settings.profile.js
+++ app/assets/javascripts/settings.profile.js
@@ -1,50 +1,21 @@
 Zrquan.module('Settings.Profile', function(Module, App, Backbone, Marionette, $, _){
     "use strict";
 
-    var astates = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-        'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-        'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-        'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-        'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-        'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-        'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-        'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-        'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming', '百度有限公司',
-        '腾讯网络科技'
-    ];
+    function findMatches(q, cb) {
+        var matches =[];
 
-    Zrquan.Ajax.request({
-        url: "/automatch/companies",
-        data: {}
-    }).then(function(result) {
-        //if (result.code == "S_OK") {
-        //
-        //}
-    });
-
-    var substringMatcher = function(strs) {
-        return function findMatches(q, cb) {
-            var matches, substrRegex;
-
-            // an array that will be populated with substring matches
-            matches = [];
-
-            // regex used to determine if a string contains the substring `q`
-            substrRegex = new RegExp(q, 'i');
-
-            // iterate through the pool of strings and for any string that
-            // contains the substring `q`, add it to the `matches` array
-            $.each(strs, function(i, str) {
-                if (substrRegex.test(str)) {
-                    // the typeahead jQuery plugin expects suggestions to a
-                    // JavaScript object, refer to typeahead docs for more info
-                    matches.push({ value: str });
-                }
-            });
-
-            cb(matches);
-        };
-    };
+        Zrquan.Ajax.request({
+            url: "/automatch/companies",
+            data: {query: q}
+        }).then(function(result) {
+            if (result['code'] == "S_OK") {
+                $.each(result['matches'], function(i, o){
+                    matches.push({ value: o.value });
+                });
+                cb(matches);
+            }
+        });
+    }
 
     $('#company').typeahead({
             hint: true,
@@ -54,6 +25,6 @@ Zrquan.module('Settings.Profile', function(Module, App, Backbone, Marionette, $,
             name: 'states',
             displayKey: 'value',
             // is compatible with the typeahead jQuery plugin
-            source: substringMatcher(astates)
+            source: findMatches
         });
 });
