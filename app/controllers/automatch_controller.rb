edd99b21de23ab0971602d043296aed844949dd2
vicky <yuqi.fan@foxmail.com>
2014-11-28 20:05:10 +0800

    自动匹配，返回匹配成功字符的关键字及其在原字符串中的下标，代码构建

--- app/controllers/automatch_controller.rb
+++ app/controllers/automatch_controller.rb
@@ -6,6 +6,7 @@ class AutomatchController < ApplicationController
   before_action :set_query_params
 
   SUPPORT_TYPE = %w('company', 'school')
+  MATCH_ARRAY_PREFIX = "o_"
   MATCH_TYPE = {
       :value => 's_v',                            #命中原字符串
       :pinyin_value => 's_py',                    #命中字符串的全拼
@@ -19,15 +20,15 @@ class AutomatchController < ApplicationController
   # return: {
   #   code: 'S_OK'  　操作成功
   #   type: 'company' 支持自动匹配的类型，见SUPPORT_TYPE
-  #   data: [] 见如下　返回的最大长度
+  #   matches: [] 见如下　返回的最大长度
   #   total: 10  匹配的总长
   #   0: {
+  #     Todo: 暂不实现同一字符串多处匹配成功的返回
   #     value: '网络易网络有限王洛的公司',
   #     ioq: 匹配的优先度，结果默认以此排序，即最前匹配的位置
-  #     Todo: matches有助于客户端标示匹配的（拼音所匹配到的条目的中文）的起始，暂不实现
-  #     matches: [{str : '网络', start: 0, end: 1},
-  #               {str : '网络', start: 3, end: 4},
-  # 　　　　　　　　{str:'王洛', start:7, end:8}]
+  #     key: '网络'
+  #     start: 0
+  #     end: 1
   #   },
   #   1:
   #     ...
@@ -65,11 +66,18 @@ class AutomatchController < ApplicationController
   def match_and_sort_terms(terms, query)
     results = []
     terms.each do |o|
-      MATCH_TYPE.each do |key, value|
-        match_success = o[value].index(query)
+      MATCH_TYPE.each do |k, v|
+        match_success = o[v].index(query)
         if match_success
-          results.push({:id => o[:id], :value => o[MATCH_TYPE[:value]], \
-          :ioq => match_success, :m_t => value})
+          value = o[MATCH_TYPE[:value]]
+          start = length = 0
+          if v == MATCH_TYPE[:value]
+            start = match_success
+            length = query.length
+          end
+          key = value[start..start + length - 1]
+          results.push({:id => o[:id], :value => value, \
+          :ioq => match_success, :m_t => v, :key=>key, :start => start, :length=>length})
           break
         end
       end
@@ -112,9 +120,12 @@ class AutomatchController < ApplicationController
       name = o.name
       obj = {:id => o.id}
       obj[MATCH_TYPE[:value]] = name
-      obj[MATCH_TYPE[:pinyin_value]] = PinYin.permlink(name, '')
-      obj[MATCH_TYPE[:pinyin_value_abbr]] = PinYin.abbr(name)
-      obj[MATCH_TYPE[:pinyin_value_abbr_except_lead]] = PinYin.abbr(name, true)
+      obj[MATCH_TYPE[:pinyin_value]] = \
+        (obj[MATCH_ARRAY_PREFIX + MATCH_TYPE[:pinyin_value]] = PinYin.of_string(name)).join('')
+      obj[MATCH_TYPE[:pinyin_value_abbr]] = \
+        (obj[MATCH_ARRAY_PREFIX + MATCH_TYPE[:pinyin_value_abbr]] = abbr(name)).join('')
+      obj[MATCH_TYPE[:pinyin_value_abbr_except_lead]] =
+          (obj[MATCH_ARRAY_PREFIX + MATCH_TYPE[:pinyin_value_abbr_except_lead]] = abbr(name, true)).join('')
       terms_for_match.push(obj)
     end
     terms_for_match
@@ -123,5 +134,15 @@ class AutomatchController < ApplicationController
   # 参数处理
   def set_query_params
     params.permit(:query, :type, :returnSize)
+  end
+
+  # overwrite from PINYIN.abbr
+  def abbr(str, except_lead=false, except_english=true)
+    result = []
+    PinYin.of_string(str).each_with_index do |word, i|
+      w = (except_lead && i == 0) || (except_english && word.english?) ? word : word[0]
+      result.push w
+    end
+    result
   end
 end
