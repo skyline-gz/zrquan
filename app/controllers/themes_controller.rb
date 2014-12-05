77c9f64b4b1137435acc02960457e906eeb479d7
James-Ouyang <dreamou@gmail.com>
2014-12-05 12:50:43 +0800

    修改了主题的创建逻辑

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -18,22 +18,34 @@ class ThemesController < ApplicationController
 
     if SUPPORT_TYPE.find { |e| /#{theme_type}/ =~ e }
 
-      ActiveRecord::Base.transaction do
-        # 检验是否有重名
-        if theme_type.constantize.find_by name: name
-          code = ReturnCode::FA_TERM_ALREADY_EXIT
-          raise ActiveRecord::Rollback
-        end
-        theme_related_obj = theme_type.constantize.create!(:name=>name, :description => description)
-        @theme = theme_related_obj.themes.create!(:name => theme_related_obj.name, \
-          :substance_id => theme_related_obj.id, :substance_type => theme_type)
+      # 此处不用事务，直接通过unique制约判断是否重复
 
+      # if theme_type.constantize.find_by name: name
+      #   code = ReturnCode::FA_TERM_ALREADY_EXIT
+      #   raise ActiveRecord::Rollback
+      # end
+
+      # 检验是否有重名(利用unique制约触发save的结果判断)
+      begin
+        theme_related_obj = theme_type.constantize.new(:name=>name, :description => description)
+        if theme_related_obj.save
+          @theme = Theme.new(:name => theme_related_obj.name, \
+                            :substance_id => theme_related_obj.id, :substance_type => theme_type)
+          if !@theme.save
+            code = ReturnCode::FA_TERM_ALREADY_EXIT
+          end
+        else
+          code = ReturnCode::FA_TERM_ALREADY_EXIT
+        end
         # 更新自动匹配相关的缓存
         if support_match(theme_type)
           add_term(theme_related_obj, theme_type)
         end
         add_term(@theme, 'Theme')
+      rescue Exception
+        code = ReturnCode::FA_TERM_ALREADY_EXIT
       end
+
 
       render :json => {:code => code || ReturnCode::S_OK}
     else
