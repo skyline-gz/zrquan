623fcb1152d1b9e9b711d289205923c4ce52df7e
vicky <yuqi.fan@foxmail.com>
2014-12-11 19:18:20 +0800

    修复取消收藏显示不正确

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,6 +3,6 @@ class Bookmark < ActiveRecord::Base
   belongs_to :bookmarkable, polymorphic: true
 
   def self.has?(user_id, type, id)
-    Bookmark.find_by(user_id: id, bookmarkable_id: id, bookmarkable_type: type)
+    Bookmark.find_by(user_id: user_id, bookmarkable_id: id, bookmarkable_type: type) != nil
   end
 end
