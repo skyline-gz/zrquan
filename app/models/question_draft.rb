6a8f4672030f0e76ca4a61b7fe934ccce9c59165
James-Ouyang <dreamou@gmail.com>
2014-12-22 17:30:06 +0800

    删除不要的model：question_draft

--- app/models/question_draft.rb
+++
@@ -1,4 +0,0 @@
-class QuestionDraft < ActiveRecord::Base
-  belongs_to :user
-  has_many :question_themes, as: :target
-end
--- spec/factories/question_drafts.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :question_draft do
-    title "MyString"
-    content "MyText"
-    user nil
-  end
-end
