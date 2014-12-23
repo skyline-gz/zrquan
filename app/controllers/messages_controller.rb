9f42756b63b3ad954439cad6a0a9aee5efdcd490
vicky <yuqi.fan@foxmail.com>
2014-12-23 18:57:48 +0800

    消息推送，代码构建

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
 
   def index
-    @messages = current_user.messages
+    # 获取根据日期（yyyy-mm-dd）排序后的哈希
+    @messages = current_user.messages.sort_by{ |q| q.created_at }.reverse!.group_by{ |message| message.created_at.to_date }
   end
+
 end
+
