4e486ec5dd7051c2c75df42743614542f16ca875
vicky <yuqi.fan@foxmail.com>
2014-12-24 00:49:10 +0800

    消息推送　代码构建

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
 
   def index
+    # 将所有未读消息设为已读
+    current_user.messages.where(read_flag: false).each{|unread_message| unread_message.update(read_flag: true)}
+
     # 获取根据日期（yyyy-mm-dd）排序后的哈希
     @messages = current_user.messages.sort_by{ |q| q.created_at }.reverse!.group_by{ |message| message.created_at.to_date }
   end
