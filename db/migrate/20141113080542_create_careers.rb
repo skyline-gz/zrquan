7be349e392620d168956fc81aafae6db30581103
James-Ouyang <dreamou@gmail.com>
2015-01-08 19:27:41 +0800

    修改了user表和careers表

--- db/migrate/20141113080542_create_careers.rb
+++ db/migrate/20141113080542_create_careers.rb
@@ -2,7 +2,6 @@ class CreateCareers < ActiveRecord::Migration
   def change
     create_table :careers do |t|
       t.references :user, index: true
-      t.references :industry, index: true
       t.references :company, index: true
       t.string :position
       t.string :entry_year
--- db/migrate/20141118113024_add_details_to_users.rb
+++ db/migrate/20141118113024_add_details_to_users.rb
@@ -7,14 +7,10 @@ class AddDetailsToUsers < ActiveRecord::Migration
     add_column :users, :gender, :integer
     add_reference :users, :location, index: true
     add_reference :users, :industry, index: true
-    add_reference :users, :latest_company, index: true
-    add_column :users, :latest_position, :string
-    add_reference :users, :latest_school, index: true
-    add_column :users, :latest_major, :string
+    add_reference :users, :latest_career, index: true
+    add_reference :users, :latest_education, index: true
     add_column :users, :description, :string
     add_column :users, :mobile, :string
-    add_column :users, :total_agree_score, :integer
-    add_column :users, :reputation, :integer
     add_column :users, :verified_flag, :boolean
     add_column :users, :avatar, :string
 
