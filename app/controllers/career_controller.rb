3e307d9adcaf89a9f739c035c194c77e8591b8cc
James-Ouyang <dreamou@gmail.com>
2015-01-23 16:58:12 +0800

    更新了用户最近的职业和教育信息的update方法

--- app/controllers/career_controller.rb
+++ app/controllers/career_controller.rb
@@ -6,7 +6,17 @@ class CareerController < ApplicationController
 
   def create
     @career = current_user.careers.new(career_params)
-    if @career.save
+    is_career_saved = @career.save
+    # 更新最新职业信息
+    latest = current_user.careers.order("leave_year desc").first
+    is_latest_career_updated = true
+    if @career.leave_year >= latest.leave_year
+      is_latest_career_updated = current_user.update(
+          latest_company_name: @career.company.name,
+          latest_position: @career.position
+      )
+    end
+    if is_career_saved and is_latest_career_updated
       # TODO 更新成功的json
     else
       # TODO 更新失败的json
@@ -18,8 +28,17 @@ class CareerController < ApplicationController
   end
 
   def update
-    is_ok = @career.update(career_params)
-    if is_ok
+    is_career_updated = @career.update(career_params)
+    # 更新最新职业信息
+    latest = current_user.careers.order("leave_year desc").first
+    is_latest_career_updated = true
+    if @career.leave_year >= latest.leave_year
+      is_latest_career_updated = current_user.update(
+          latest_company_name: @career.company.name,
+          latest_position: @career.position
+      )
+    end
+    if is_career_updated and is_latest_career_updated
       # TODO 更新成功的json
     else
       # TODO 更新失败的json
--- app/controllers/education_controller.rb
+++ app/controllers/education_controller.rb
@@ -6,7 +6,17 @@ class EducationController < ApplicationController
 
   def create
     @education = current_user.educations.new(education_params)
-    if @education.save
+    is_education_saved = @education.save
+    # 更新最新教育信息
+    latest = current_user.educations.order("graduate_year desc").first
+    is_latest_edu_updated = true
+    if @education.graduate_year >= latest.graduate_year
+      is_latest_edu_updated = current_user.update(
+          latest_school_name: @education.school.name,
+          latest_major: @education.major
+      )
+    end
+    if is_education_saved and is_latest_edu_updated
       # TODO 更新成功的json
     else
       # TODO 更新失败的json
@@ -17,8 +27,17 @@ class EducationController < ApplicationController
   end
 
   def update
-    is_ok = @education.update(education_params)
-    if is_ok
+    is_edu_updated = @education.update(education_params)
+    # 更新最新教育信息
+    latest = current_user.educations.order("graduate_year desc").first
+    is_latest_edu_updated = true
+    if @education.graduate_year >= latest.graduate_year
+      is_latest_edu_updated = current_user.update(
+          latest_school_name: @education.school.name,
+          latest_major: @education.major
+      )
+    end
+    if is_edu_updated and is_latest_edu_updated
       # TODO 更新成功的json
     else
       # TODO 更新失败的json
