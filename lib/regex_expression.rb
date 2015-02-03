628f3f37eeab0dcd3f53dee272180fa8805b295b
James-Ouyang <dreamou@gmail.com>
2015-02-03 17:13:18 +0800

    修改了正则表达的小问题

--- lib/regex_expression.rb
+++ lib/regex_expression.rb
@@ -1,4 +1,5 @@
 module RegexExpression
-  MOBILE = /^1[0-9]{10}$/             #1开头的11位数字
+  # https://ruby-china.org/topics/410 尽量不要用^和$来判断字符串的开始和结尾,用\A和\z
+  MOBILE = /\A1[0-9]{10}\z/             #1开头的11位数字
   PASSWORD = /\A[a-zA-Z0-9]{8,20}\z/       #8～20位的字符和数字
 end
